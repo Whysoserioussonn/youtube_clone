8f82f26a8a7b4fce1cf3d3f02a6594d12129e0d8
gzod1016 <60858393+gzod1016@users.noreply.github.com>
2022-10-25 11:49:41 -0400

    gutted out comments from index.html in public folder, appended roboto and icon font to file there, react up and running

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client" // append client to react-som this was changed in v18 of react
 import App from "./App";
+
+const root =ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<App />);
