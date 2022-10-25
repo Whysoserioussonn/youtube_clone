cf0a6fd86fa230002db609e782901de8e773df44
gzod1016 <60858393+gzod1016@users.noreply.github.com>
2022-10-25 12:27:06 -0400

    work on App.js structure and create routees for video, channel, and search, also add a navbar component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client" // append client to react-som this was changed in v18 of react
 import App from "./App";
+import './index.css';
 
 const root =ReactDOM.createRoot(document.getElementById('root'));
 
